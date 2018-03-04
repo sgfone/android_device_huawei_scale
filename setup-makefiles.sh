d9312db1e84ce0e9c86a1d9fe0a363b18233af36
Runner85sx <runner85sx@gmail.com>
2018-03-04 10:50:49 +0100

    Replace vendor/cm with vendor/lineage

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -28,9 +28,9 @@ export VENDOR=huawei
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -38,7 +38,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" "true"
 
 # Copyright headers and guards
 write_headers "scale"
@@ -65,7 +65,7 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 
     # Copyright headers and guards
     write_headers
