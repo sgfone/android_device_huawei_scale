1a2bca5fbf60374dbcef04be4537e3de1be45376
Runner85sx <runner85sx@gmail.com>
2018-03-04 13:33:35 +0100

    Fix typo

--- device.mk
+++ device.mk
@@ -81,7 +81,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl.legacy \
     camera.device@3.2-impl \
-    camera.device@1.0-impl.legacy
+    camera.device@1.0-impl.legacy \
     camera.msm8909 \
     Snap
 
